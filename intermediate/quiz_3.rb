c71f04a50ffbe35f3071d1751c4b45c1d5838bac
Aaron Saloff <aaronsaloff@gmail.com>
2015-04-26 17:50:27 -0400

    Intermediate Quiz 3

---
+++ intermediate/quiz_3.rb
@@ -0,0 +1,23 @@
+# quiz_3
+
+# 1.
+
+# reviewed
+
+# 2.
+
+# reviewed
+
+# 3.
+
+
+# 4.
+
+# My string looks like this now: rutabaga
+# My array looks like this now: ["pumpkins"]
+
+# 5.
+
+def color_valid(color)
+  color == "blue" || color == "green"
+end
